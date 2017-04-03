2b6d1414b24865776f1ce37cc70b7ba03af728f5
nachogarrone <nachogarrone@gmail.com>
2017-04-03 20:45:13 -0300

    ramp users

--- src/test/scala/computerdatabase/BasicSimulation.scala
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -3,6 +3,8 @@ package computerdatabase
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
+import scala.concurrent.duration._
+
 class BasicSimulation extends Simulation {
 
   val httpConf = http
@@ -12,40 +14,22 @@ class BasicSimulation extends Simulation {
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
-  //  val scn = scenario("Busqueda IPHONE") // A scenario is a chain of requests and pauses
-  //    .exec(
-  //    http("DA RESULTADOS")
-  //      .get("/index.php?route=product/search&search=iphone")
-  //      .check(status.is(200))
-  //      .check(substring("There is no product that matches the search criteria").notExists)
-  //  )
-  //    .pause(1)
-  //    .exec(
-  //      http("NO DA RESULTADOS")
-  //        .get("/index.php?route=product/search&search=iphonasdasdasde")
-  //        .check(status.is(200))
-  //        .check(substring("There is no product that matches the search criteria").notExists)
-  //    )
-
-
   val sets = csv("data.csv")
   val queue_size = sets.records.size
   val count = new java.util.concurrent.atomic.AtomicInteger(0)
 
   val scn = scenario("BUSQUEDA PRODUCTOS CSV")
-    .asLongAs(_ => count.getAndIncrement() < queue_size) {
-      feed(sets)
+    .during(15 minutes) {
+      feed(sets.circular)
         .exec(
           http("request_csv")
             .get("/index.php?route=product/search&search=${search}")
             .check(status.is(200))
             .check(substring("There is no product that matches the search criteria").notExists)
         )
+        .pause(2 second)
     }
 
 
-  setUp(scn.inject(atOnceUsers(1)).protocols(httpConf))
-  //  setUp(scn2.inject(atOnceUsers(1)).protocols(httpConf))
-
-
+  setUp(scn.inject(rampUsers(20) over (5 minutes)).protocols(httpConf))
 }
