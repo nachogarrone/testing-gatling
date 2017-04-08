3427024cbd1853b1ed5cc604f18a5c66ccb93b41
mtuduri <tuduri.matias@gmail.com>
2017-04-08 14:38:47 -0300

    codigo mas prolijo

--- src/test/scala/opencart/BasicSimulation.scala
+++ src/test/scala/opencart/BasicSimulation.scala
@@ -1,4 +1,4 @@
-package computerdatabase
+package opencart
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
@@ -9,14 +9,14 @@ class BasicSimulation extends Simulation {
 
   val httpConf = http
     .baseURL("http://opencart.abstracta.us") // Here is the root for all relative URLs
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
   val sets = csv("data.csv")
   val queue_size = sets.records.size
-  val count = new java.util.concurrent.atomic.AtomicInteger(0)
+
 
   val scn = scenario("BUSQUEDA PRODUCTOS CSV")
     .during(15 minutes) {
@@ -26,10 +26,14 @@ class BasicSimulation extends Simulation {
             .get("/index.php?route=product/search&search=${search}")
             .check(status.is(200))
             .check(substring("There is no product that matches the search criteria").notExists)
+            .check(regex("""<div class="product-thumb">""").count.greaterThan(0))
         )
         .pause(2 second)
     }
 
+  // 1 usuario
+  setUp(scn.inject(atOnceUsers(1)).protocols(httpConf))
 
+  // 20 usuarios
   setUp(scn.inject(rampUsers(20) over (5 minutes)).protocols(httpConf))
 }
